9fb1fd7b559533a0f09161580c007671c90907c3
ViteKhan <42898182+ViteKhan@users.noreply.github.com>
2023-06-25 00:33:59 +0500

    added user-dto

---
+++ server/dto/user-dto.js
@@ -0,0 +1,10 @@
+module.exports = class UserDto {
+  id;
+  email;
+  isActivated;
+  constructor(model) {
+    this.id = model._id;
+    this.email = model.email;
+    this.isActivated = model.isActivated;
+  }
+}
