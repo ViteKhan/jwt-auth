c9240d3aa17a768fe3523dfdb2db78d2b3f22165
ViteKhan <42898182+ViteKhan@users.noreply.github.com>
2023-07-02 20:49:13 +0500

    addee middleware for error handling and added login function

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,31 +1,45 @@
 const userService = require('../service/user-service');
+const ApiError = require('../excerptions/api-error');
+const { validationResult } = require('express-validator');
 
 class UserController {
   async registration(request, response, next) {
     try {
+      const errors = validationResult(request);
+
+      if (!errors.isEmpty()) {
+        return next(ApiError.BadRequest('Validation error', errors.array()));
+      }
+
       const { email, password } = request.body;
       const userData = await userService.registration(email, password);
       const maxAge = 30 * 24 * 60 * 60 * 1000;
       response.cookie('refreshToken', userData.refreshToken, { maxAge, httpOnly: true });
+
       return response.json(userData);
     } catch (e) {
-      console.log(e);
+      next(e);
     }
   }
 
   async login(request, response, next) {
     try {
+      const { email, password } = request.body;
+      const userData = await userService.login(email, password);
+      const maxAge = 30 * 24 * 60 * 60 * 1000;
+      response.cookie('refreshToken', userData.refreshToken, { maxAge, httpOnly: true });
 
+      return response.json(userData);
     } catch (e) {
-
+      next(e);
     }
   }
 
   async logout(request, response, next) {
     try {
-
+      const
     } catch (e) {
-
+      next(e);
     }
   }
 
@@ -35,7 +49,7 @@ class UserController {
       await userService.activate(activationLink);
       return response.redirect(process.env.CLIENT_URL);
     } catch (e) {
-      console.log(e);
+      next(e);
     }
   }
 
@@ -43,7 +57,7 @@ class UserController {
     try {
 
     } catch (e) {
-
+      next(e);
     }
   }
 
@@ -51,7 +65,7 @@ class UserController {
     try {
       response.json([1, 2]);
     } catch (e) {
-
+      next(e);
     }
   }
 
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -4,13 +4,14 @@ const uuid = require('uuid');
 const mailService = require('./mail-service');
 const tokenService = require('./token-service');
 const UserDto = require('../dto/user-dto');
+const ApiError = require('../excerptions/api-error');
 
 class UserService {
   async registration(email, password) {
     const candidate = await UserModel.findOne({ email });
 
     if (candidate) {
-      throw new Error(`User with this email(${email}) already exists`);
+      throw ApiError.UnauthorizedError(`User with this email(${email}) already exists`);
     }
 
     const hashPassword = await bcrypt.hash(password, 3);
@@ -30,12 +31,32 @@ class UserService {
     const user = await UserModel.findOne({ activationLink });
 
     if (!user) {
-      throw new Error('Invalid activation link');
+      throw ApiError.BadRequest('Invalid activation link');
     }
 
     user.isActivated = true;
 
     await user.save();
+  }
+
+  async login(email, password) {
+      const user = await UserModel.findOne({ email });
+
+      if (!user) {
+        throw ApiError.BadRequest('User is not found');
+      }
+
+      const isPasswordsEqual = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordsEqual) {
+        throw ApiError.BadRequest('Incorrect password');
+      }
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
   }
 }
 
